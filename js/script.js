212b043f2d417c77a2232dd5dace0f8312e9ccdc
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-09-16 19:01:22 +0600

    city name added and design added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// const url = 'https://weatherapi-com.p.rapidapi.com/forecast.json?q=Dhaka&days=3';
+
 const options = {
     method: 'GET',
     headers: {
@@ -9,39 +9,82 @@ const options = {
 
 
 
-// const weather=(city)=>{
-// cityName.innerHTML=city
-fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=Dhaka&days=3', options).then(res => res.json()).then(data => {
-    console.log(data)
-    console.log(data.forecast.forecastday)
-    // console.log(data.forecast.forecastday[0].astro.sunrise)
-    temp_c.innerHTML = data.current.temp_c
-    city.innerHTML = data.location.name
-    country.innerHTML = data.location.country
-    wind_kph.innerHTML = data.current.wind_kph
-    wind_mph.innerHTML = data.current.wind_mph
-    humidity.innerHTML = data.current.humidity
-    cloud.innerHTML = data.current.cloud
-    text.innerHTML = data.current.condition.text
-    feelslike_c.innerHTML = data.current.feelslike_c
-    sunrise1.innerHTML = data.forecast.forecastday[0].astro.sunrise
-    sunrise2.innerHTML = data.forecast.forecastday[1].astro.sunrise
-    sunrise3.innerHTML = data.forecast.forecastday[2].astro.sunrise
-    sunset1.innerHTML = data.forecast.forecastday[0].astro.sunset
-    sunset2.innerHTML = data.forecast.forecastday[1].astro.sunset
-    sunset3.innerHTML = data.forecast.forecastday[2].astro.sunset
-    date1.innerHTML = data.forecast.forecastday[0].date
-    date2.innerHTML = data.forecast.forecastday[1].date
-    date3.innerHTML = data.forecast.forecastday[2].date
-    avg_temp1.innerHTML = data.forecast.forecastday[0].day.avgtemp_c
-    avg_temp2.innerHTML = data.forecast.forecastday[1].day.avgtemp_c
-    avg_temp3.innerHTML = data.forecast.forecastday[2].day.avgtemp_c
-    // for (let i=0 ; i<data.forecast.forecastday[i]; i++){
-    //     sunrise.innerHTML=data.forecast.forecastday[i].astro.sunrise
-    // }
-    // max_temp.innerHTML= data.
-    // min_temp.innerHTML= data.
-})
-// }
+const weather = (city) => {
+    cityName.innerHTML = city
+    fetch('https://weatherapi-com.p.rapidapi.com/forecast.json?q=' + city + '&days=3', options)
+        .then(res => res.json())
+        .then(data => {
+            temp_c.innerHTML = data.current.temp_c
+            wind_kph.innerHTML = data.current.wind_kph
+            wind_mph.innerHTML = data.current.wind_mph
+            humidity.innerHTML = data.current.humidity
+            cloud.innerHTML = data.current.cloud
+            text.innerHTML = data.current.condition.text
+            feelslike_c.innerHTML = data.current.feelslike_c
+            sunrise1.innerHTML = data.forecast.forecastday[0].astro.sunrise
+            sunrise2.innerHTML = data.forecast.forecastday[1].astro.sunrise
+            sunrise3.innerHTML = data.forecast.forecastday[2].astro.sunrise
+            sunset1.innerHTML = data.forecast.forecastday[0].astro.sunset
+            sunset2.innerHTML = data.forecast.forecastday[1].astro.sunset
+            sunset3.innerHTML = data.forecast.forecastday[2].astro.sunset
+            date1.innerHTML = data.forecast.forecastday[0].date
+            date2.innerHTML = data.forecast.forecastday[1].date
+            date3.innerHTML = data.forecast.forecastday[2].date
+            avg_temp1.innerHTML = data.forecast.forecastday[0].day.avgtemp_c
+            avg_temp2.innerHTML = data.forecast.forecastday[1].day.avgtemp_c
+            avg_temp3.innerHTML = data.forecast.forecastday[2].day.avgtemp_c
+
+            if (data.current.condition.text == "Partly Cloudy") {
+                weatherImage.src = "images/cloudy.png"
+            }
+            else if (data.current.condition.text == "Sunny") {
+                weatherImage.src = "images/sun.png"
+            }
+            else if (data.current.condition.text == "Windy") {
+                weatherImage.src = "images/wind.png"
+            }
+            else if (data.current.condition.text == "Stormy") {
+                weatherImage.src = "images/storm.png"
+            }
+            else if (data.current.condition.text == "Rainy") {
+                weatherImage.src = "images/rainy.png"
+            }
+
+
+        })
+        .catch(err => {
+            console.log(err)
+        })
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const submitButton = document.getElementById("submit");
+
+    submitButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        weather(city.value);
+    });
+
+    weather("Dhaka");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//todo
 
 
